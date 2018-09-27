8765052107948dca602f32d221630caa5a64cb08
Milan Drazic <drazic.milan@gmail.com>
2018-09-28 01:41:15 +0200

    upload all

---
+++ JS/main.js
@@ -0,0 +1,67 @@
+$(document).ready(() => {
+	
+	var background = ["img/mechanics.jpg", "img/people.jpg", "img/showcase.jpg", "img/webdesign.jpg"];
+	
+	$('#btn').on('click', () => {
+		$('#model').show();
+		$('.backg1').css('background-image', 'url('+ background[0] +')')
+		$('.backg2').css('background-image', 'url('+ background[1] +')')
+		$('.backg3').css('background-image', 'url('+ background[2] +')')
+		$('.backg4').css('background-image', 'url('+ background[3] +')')
+	});
+	
+	$('.new-backgrounds > div').click(function() {
+		$(this).toggleClass('add-border');
+		$(this).siblings().removeClass('add-border');
+	});
+	
+	$('#save-button').on('click', () => {
+		
+	var newIntroText = $('#intro-text').val();
+	var newIntroTextColor = $('#intro-text-color').val();
+				
+	var newLogoTitle = $('#logo-title').val();
+	var newLogoColor = $('#logo-color').val();
+//		
+		$('.intro-text > h1').text(newIntroText);
+		$('.intro-text > h1').css('color', newIntroTextColor);
+//		
+		$('#branding > a span.highlight').text(newLogoTitle);
+		$('#branding > a span.highlight').css('color', newLogoColor);
+//		
+		if($('.backg1').hasClass('add-border')) {
+			$('#showcase').css('background-image', 'url(' + background[0] + ')');
+		}else if($('.backg2').hasClass('add-border')) {
+			$('#showcase').css('background-image', 'url(' + background[1] + ')');
+		}else if($('.backg3').hasClass('add-border')) {
+			$('#showcase').css('background-image', 'url(' + background[2] + ')');
+		}else {
+			$('#showcase').css('background-image', 'url(' + background[3] + ')');
+		};
+			$('#model').hide();			
+		});
+	
+	
+});
+
+// Side Menu
+		function openSlideMenu() {
+			var nav = document.getElementById('mobile-nav');
+			var wrap = document.getElementById('wrap');
+			var stack = document.getElementById('body');
+			nav.style.width = '250px';
+			wrap.style.display = 'block';
+			stack.style.overflowY = 'hidden';
+			
+			window.addEventListener('mouseup', function(event) {		
+				if(event.target != nav && event.target.parentNode != nav) {
+					wrap.style.display = 'none';
+					nav.style.width = '';
+					stack.style.overflowY = '';
+				}
+			})
+			
+		};
+
+	
+
---
+++ README.txt
@@ -0,0 +1,1 @@
+*** My first application with JavaScript *** You can simply modify web pages like an example title, colour or image. link:
